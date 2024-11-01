eae8c5d87607b684622cf9031dfc8eed40176e7f
jguimeram <jguimera54@gmail.com>
2024-11-01 17:03:32 +0100

    set time travel

--- src/App.jsx
+++ src/App.jsx
@@ -6,9 +6,9 @@ function App() {
 
 
     const [x, setX] = useState(true)
-
     /* an array with a single item, which itself is an array of 9 nulls */
     const [history, setHistory] = useState([Array(9).fill(null)])
+    const [currentMove, setCurrentMove] = useState(0)
     /* read the last squares array from the history to render the current move */
     const currentSquares = history[history.length - 1]
 
@@ -18,13 +18,34 @@ function App() {
         setX(!x) //flip with X or O 
     }
 
+    /*use map to transform the history of moves into React elements representing buttons on the screen, and display a list of buttons to “jump” to past moves.*/
+
+    const moves = history.map((squares, move) => {
+        let description
+        if (move > 0) {
+            description = `Go to move ${move}`
+        } else {
+            description = `Go  to game start`
+        }
+        return (
+            <li key={move}>
+                <button onClick={() => jumpTo(move)} >{description}</button>
+            </li>
+        )
+    })
+
+    function jumpTo(nextMove) {
+        setCurrentMove(nextMove)
+        setX(nextMove % 2 === 0) //check if next move is X or O (true (X) or false (O))
+    }
+
     return (
         <div className="game">
             <div className="game-board">
                 <Board x={x} squares={currentSquares} onPlay={handlePlay} />
             </div>
             <div className="game-info">
-                <ol>{/*TODO*/}</ol >
+                <ol>{moves}</ol >
             </div >
         </div >
     );
